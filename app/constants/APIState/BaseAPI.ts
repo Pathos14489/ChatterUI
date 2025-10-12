f50e9889b1b4c53991b13b64068d2c4a0e637e38
Pathos <29135741+Pathos14489@users.noreply.github.com>
2025-10-12 09:42:49 -0700

    redid prompt processing, partially added lorebook support but it doesn't work yet

--- app/constants/APIState/BaseAPI.ts
+++ app/constants/APIState/BaseAPI.ts
@@ -55,6 +55,22 @@ export abstract class APIBase implements IAPIBase {
         return sequence
     }
 
+    getLoreBookIndex = (characterCard: any) => {
+        const lorebook = characterCard?.data?.character_book
+        if (!lorebook) return {}
+        let index: { [key: string]: any } = {}
+        lorebook.entries.forEach((entry:any) => {
+            if (!entry.enabled) return
+            entry.keys.forEach((key:string) => {
+                index[key.toLowerCase()] = entry
+            })
+            entry.secondary_keys?.forEach((key:string) => {
+                index[key.toLowerCase()] = entry
+            })
+        })
+        return index
+    }
+
     buildTextCompletionContext = (max_length: number) => {
         const delta = performance.now()
 
@@ -76,8 +92,11 @@ export abstract class APIBase implements IAPIBase {
         const userCache = Characters.useUserCard.getState().getCache(charName)
         const instructCache = Instructs.useInstruct.getState().getCache(charName, userName)
 
-        const user_card_data = (userCard?.data?.description ?? '').trim()
-        const char_card_data = (currentCard?.data?.description ?? '').trim()
+        const user_card_description = (userCard?.data?.description ?? '').trim()
+        const char_card_description  = (currentCard?.data?.description ?? '').trim()
+        const char_card_personality = (currentCard?.data?.personality ?? '').trim()
+        const char_card_scenario = (currentCard?.data?.scenario ?? '').trim()
+        const char_card_post_history_instructions = (currentCard?.data?.post_history_instructions ?? '').trim()
         let payload = ``
 
         // set suffix length as its always added
@@ -91,12 +110,24 @@ export abstract class APIBase implements IAPIBase {
             payload += `${currentInstruct.system_prompt}`
             payload_length += instructCache.system_prompt_length
         }
-        if (char_card_data) {
-            payload += char_card_data
+        if (char_card_description) {
+            payload += `\n${char_card_description}`
             payload_length += characterCache.description_length
         }
-        if (user_card_data) {
-            payload += user_card_data
+        if (char_card_personality) {
+            payload += `\n${char_card_personality}`
+            payload_length += characterCache.personality_length
+        }
+        if (char_card_scenario) {
+            payload += `\n${char_card_scenario}`
+            payload_length += characterCache.scenario_length
+        }
+        if (char_card_post_history_instructions) {
+            // Added at the end.
+            payload_length += characterCache.post_history_instructions_length
+        }
+        if (user_card_description) {
+            payload += `\n${user_card_description}`
             payload_length += userCache.description_length
         }
         // suffix must be delayed for example messages
@@ -122,16 +153,11 @@ export abstract class APIBase implements IAPIBase {
              *  context limit, especially for local gens to prevent truncation
              * **/
 
-            let instruct_len = message.is_user
-                ? instructCache.input_prefix_length
-                : instructCache.output_suffix_length
+            let instruct_len = message.is_user ? instructCache.input_prefix_length : instructCache.output_suffix_length
 
             // for last message, we want to skip the end token to allow the LLM to generate
 
-            if (!is_last)
-                instruct_len += message.is_user
-                    ? instructCache.input_suffix_length
-                    : instructCache.output_suffix_length
+            if (!is_last) instruct_len += message.is_user ? instructCache.input_suffix_length : instructCache.output_suffix_length
 
             const timestamp_string = `[${swipe_data.send_date.toString().split(' ')[0]} ${swipe_data.send_date.toLocaleTimeString()}]\n`
             const timestamp_length = currentInstruct.timestamp ? tokenizer(timestamp_string) : 0
@@ -139,8 +165,7 @@ export abstract class APIBase implements IAPIBase {
             const name_string = `${message.name} :`
             const name_length = currentInstruct.names ? tokenizer(name_string) : 0
 
-            const shard_length =
-                swipe_len + instruct_len + name_length + timestamp_length + wrap_length
+            const shard_length = swipe_len + instruct_len + name_length + timestamp_length + wrap_length
 
             // check if within context window
 
@@ -172,16 +197,18 @@ export abstract class APIBase implements IAPIBase {
             is_last = false
             message_acc_length += shard_length
             message_acc = message_shard + message_acc
+            // if (index === messages.length - 1) { // very first message, add suffix length TODO: redo text prompt construction to be more like my message_formatter class in py and fix this later
+            //     // Add post history instructions if exists
+            //     if (char_card_post_history_instructions) {
+            //         message_acc += `\n${char_card_post_history_instructions}`
+            //         message_acc_length += characterCache.post_history_instructions_length
+            //     }
+            // }
             index--
         }
 
         const examples = currentCard.data?.mes_example
-        if (
-            first_message_reached &&
-            currentInstruct.examples &&
-            examples &&
-            message_acc_length + payload_length + characterCache.examples_length < max_length
-        ) {
+        if (first_message_reached && currentInstruct.examples && examples && message_acc_length + payload_length + characterCache.examples_length < max_length) {
             payload += examples
             message_acc_length += characterCache.examples_length
         }
@@ -203,65 +230,161 @@ export abstract class APIBase implements IAPIBase {
         assistantRole = 'assistant',
         contentName = 'content'
     ) => {
-        const tokenizer =
-            mmkv.getString(Global.APIType) === API.LOCAL
-                ? Llama.useLlama.getState().tokenLength
-                : Tokenizer.useTokenizer.getState().getTokenCount
+        const startTime = performance.now()
+        const tokenizer = mmkv.getString(Global.APIType) === API.LOCAL ? Llama.useLlama.getState().tokenLength : Tokenizer.useTokenizer.getState().getTokenCount
 
         const messages = [...(Chats.useChat.getState().data?.messages ?? [])]
-        const userCard = { ...Characters.useUserCard.getState().card }
-        const currentCard = { ...Characters.useCharacterCard.getState().card }
+
+
         const currentInstruct = Instructs.useInstruct.getState().replacedMacros()
 
+        const userCard = { ...Characters.useUserCard.getState().card }
+        const currentCard = { ...Characters.useCharacterCard.getState().card }
         const userName = userCard.data?.name ?? ''
         const charName = currentCard.data?.name ?? ''
+        Logger.log(`Building chat context for ${charName}: ${currentCard}`)
 
         const characterCache = Characters.useCharacterCard.getState().getCache(userName)
         const userCache = Characters.useUserCard.getState().getCache(charName)
         const instructCache = Instructs.useInstruct.getState().getCache(charName, userName)
 
-        const buffer = Chats.useChat.getState().buffer
+        const user_card_description = (userCard?.data?.description ?? '').trim()
+        const char_card_description  = (currentCard?.data?.description ?? '').trim()
+        const char_card_personality = (currentCard?.data?.personality ?? '').trim()
+        const char_card_scenario = (currentCard?.data?.scenario ?? '').trim()
+        const char_card_post_history_instructions = (currentCard?.data?.post_history_instructions ?? '').trim()
+        // set suffix length as its always added
+
+        let buffer = Chats.useChat.getState().buffer
+        if (currentInstruct.timestamp) {
+            const timestamp_string = `[${new Date().toString().split(' ')[0]} ${new Date().toLocaleTimeString()}] `
+            buffer = timestamp_string + buffer
+        }
+
+        if (currentInstruct.names) {
+            const name_string = `${currentCard.data?.name}: `
+            buffer = name_string + buffer
+        }
+        // Add buffer to length
+        let buffer_length = tokenizer(buffer)
+
+        Logger.log(`Current buffer length: ${tokenizer(buffer)}, buffer: ${buffer}`)
 
         // Logic here is that if the buffer is empty, this is not a regen, hence can popped
-        if (!buffer) messages.pop()
-        const initial = `${currentInstruct.system_prompt}
-        \n${userCard?.data?.description ?? ''}
-        \n${currentCard?.data?.description ?? ''}`
+        // if (!buffer) 
+        Logger.log("Removed message: "+messages.pop())
 
-        let total_length =
-            instructCache.system_prompt_length +
-            characterCache.description_length +
-            userCache.description_length
-        const payload = [{ role: systemRole, content: replaceMacros(initial) }]
-        const messageBuffer = []
+        let initial_content = ""
+        let payload_length = instructCache.system_prompt_length + buffer_length
+        if (currentInstruct.system_prompt) {
+            initial_content += `${currentInstruct.system_prompt}`
+            payload_length += characterCache.description_length
+        }
+        if (char_card_description) {
+            initial_content += `\n${char_card_description}`
+            payload_length += characterCache.description_length
+        }
+        if (char_card_personality) {
+            initial_content += `\n${char_card_personality}`
+            payload_length += characterCache.personality_length
+        }
+        if (char_card_scenario) {
+            initial_content += `\n${char_card_scenario}`
+            payload_length += characterCache.scenario_length
+        }
+        if (char_card_post_history_instructions) {
+            // Added at the end.
+            payload_length += characterCache.post_history_instructions_length
+        }
+        if (user_card_description) {
+            initial_content += `\n${user_card_description}`
+            payload_length += userCache.description_length
+        }
+        const initial_message = { role: systemRole, content: replaceMacros(initial_content.trim()) }
+        
+        let payload = [initial_message]
+        const messageBuffer: { role: string; content: string }[] = []
+        const lorebookBuffer: { role: string; content: string }[] = []
+        let lorebookBudget = currentCard.data?.character_book?.token_budget ?? 0
 
         let index = messages.length - 1
         for (const message of messages.reverse()) {
+            const swipe_len = Chats.useChat.getState().getTokenCount(index)
             const swipe_data = message.swipes[message.swipe_id]
-            // special case for claude, prefill may be useful!
-            const timestamp_string = `[${swipe_data.send_date.toString().split(' ')[0]} ${swipe_data.send_date.toLocaleTimeString()}]\n`
-            const timestamp_length = currentInstruct.timestamp ? tokenizer(timestamp_string) : 0
 
-            const name_string = `${message.name} :`
-            const name_length = currentInstruct.names ? tokenizer(name_string) : 0
+            let message_content = ""
+            let shard_len = 0
 
-            const len =
-                Chats.useChat.getState().getTokenCount(index) +
-                total_length +
-                name_length +
-                timestamp_length
-            if (len > max_length) break
+            if (currentInstruct.timestamp) {
+                const timestamp_string = `[${swipe_data.send_date.toString().split(' ')[0]} ${swipe_data.send_date.toLocaleTimeString()}] `
+                const timestamp_length = currentInstruct.timestamp ? tokenizer(timestamp_string) : 0
+                message_content += timestamp_string
+                shard_len += timestamp_length
+            }
+
+            if (currentInstruct.names) {
+                const name_string = `${message.name}: `
+                const name_length = currentInstruct.names ? tokenizer(name_string) : 0
+                message_content += name_string
+                shard_len += name_length
+            }
+
+            message_content += replaceMacros(message.swipes[message.swipe_id].swipe)
+            shard_len += swipe_len
+
+            if (shard_len > max_length) break
+            payload_length += shard_len
+            // if lorebook exists, we check for keys in the message
+            if (lorebookBudget > 0 && currentCard.data?.character_book) {
+                const loreIndex = this.getLoreBookIndex(currentCard)
+                Object.keys(loreIndex).forEach((key) => {
+                    if (message_content.toLowerCase().includes(key)) {
+                        const entry = loreIndex[key]
+                        const entry_length = tokenizer(entry.content)
+                        if (entry_length <= lorebookBudget && entry_length + payload_length <= max_length) {
+                            lorebookBuffer.push({
+                                role: systemRole,
+                                content: replaceMacros(entry.content),
+                            }) // TODO: Use entry settings to modify position 
+                            lorebookBudget -= entry_length
+                            payload_length += entry_length
+                            Logger.log(`Added lorebook entry for key: ${key}`)
+                        }
+                    }
+                })
+            }
             messageBuffer.push({
                 role: message.is_user ? userRole : assistantRole,
-                content: replaceMacros(message.swipes[message.swipe_id].swipe),
+                content: message_content,
             })
-            total_length += len
             index--
         }
-        const output = [...payload, ...messageBuffer.reverse()]
 
-        if (mmkv.getBoolean(AppSettings.PrintContext)) Logger.log(JSON.stringify(output))
-        return output
+        Logger.log(`Final context length: ${payload_length}, max allowed: ${max_length}`)
+        // construct final payload
+        // lorebook entries always first
+        // then messages
+        // then buffer
+        Logger.log(`Added ${lorebookBuffer.length} lorebook entries, ${messageBuffer.length} messages`)
+        
+        lorebookBuffer.reverse().forEach((item) => payload.push(item))
+        messageBuffer.reverse().forEach((item, index) => {
+            // if last message, add post_history_instructions before it if exists
+            if (index === messageBuffer.length - 1 && char_card_post_history_instructions.trim() !== '') {
+                payload.push({
+                    role: systemRole,
+                    content: replaceMacros(char_card_post_history_instructions.trim()),
+                })
+            }
+            payload.push(item)
+        })
+
+        // if (mmkv.getBoolean(AppSettings.PrintContext)) Logger.log(JSON.stringify(payload))
+        Logger.log(`Approximate Context Size: ${payload_length} tokens | ${(performance.now() - startTime).toFixed(2)}ms taken to build context`)
+        return {
+            messages: payload,
+            buffer: buffer,
+        }
     }
 
     readableStreamResponse = async (
